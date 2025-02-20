356a61b7c2942b4f878dc53aceb3bd5c9e3215fe
daniel <danielnwachukwu5738@gmail.com>
2025-02-20 14:55:03 +0100

    first commit

---
+++ data_management_service/asgi.py
@@ -0,0 +1,10 @@
+import os
+
+import django
+from decouple import config
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
+django.setup()
+
+application = get_asgi_application()
